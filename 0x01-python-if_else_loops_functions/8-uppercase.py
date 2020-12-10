ed3a6cdf745d02a754db981408b42bdc16085580
jdanielue <jdurregoe@gmail.com>
2020-12-10 05:15:41 +0000

    a0s8 script copleted

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def uppercase(str):
+    strm = ""
+    for i in range(len(str)):
+        if 97 <= ord(str[i]) <= 122:
+            strm += chr(ord(str[i]) - 32)
+        else:
+            strm += str[i]
+    print(strm)
