d54d3e84f1b8a2f0f0ca17b69a839e0f1fb23cf2
jdanielue <jdurregoe@gmail.com>
2020-12-10 23:32:41 +0000

    a2s0 program working properly with the script as suma and if __name__ == __main__:

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add as suma
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, suma(a, b)))
+if __name__ == "__main__":
+    from add_0 import add as suma
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, suma(a, b)))
