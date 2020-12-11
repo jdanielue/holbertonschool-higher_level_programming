008dca058ea9980553511b5fc4d06ef4a6469eab
jdanielue <jdurregoe@gmail.com>
2020-12-11 03:07:30 +0000

    a1s3 so far so good

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,9 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("0")
-    else:
+        sys.exit(0)
+    if len(sys.argv) > 1:
         result = 0
         for i in range(1, len(sys.argv)):
             result = int(sys.argv[i]) + result
-    print("{}".format(result))
+print("{}".format(result))
