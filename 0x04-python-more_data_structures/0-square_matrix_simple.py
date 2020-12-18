29df18a71e5d2913cadabb448025040e29d68e55
jdanielue <jdurregoe@gmail.com>
2020-12-18 02:40:19 +0000

    s0a0 all done

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+        matrizala2 = []
+        for row in matrix:
+                dato = []
+                for col in row:
+                        dato.append(col ** 2)
+                matrizala2.append(dato)
+        return matrizala2
