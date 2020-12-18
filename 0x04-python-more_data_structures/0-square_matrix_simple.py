e31e5e138aa261915d6775de843fba97e4ccb967
jdanielue <jdurregoe@gmail.com>
2020-12-18 02:51:57 +0000

    s0s1a1 all working properly

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
         matrizala2 = []
         for row in matrix:
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,4 @@
 def search_replace(my_list, search, replace):
-        return list(map(lambda x : replace if (x == search) else x, my_list))
+        newlist = []
+        newlist = list(map(lambda x : replace if (x == search) else x, my_list))
+        return newlist
