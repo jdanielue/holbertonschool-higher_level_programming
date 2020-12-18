76cedef0ebb930d6163f33e74f7b7245a7b774c6
jdanielue <jdurregoe@gmail.com>
2020-12-18 12:05:55 +0000

    s6a1 spaces fixed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
         sorted_dict = sorted(a_dictionary)
         for i in sorted_dict:
-                print("{} : {}".format(i, a_dictionary[i]))
+                print("{}: {}".format(i, a_dictionary[i]))
