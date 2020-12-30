07bc9989039a0ec298af75db2e6eb5259fda8d0a
jdanielue <jdurregoe@gmail.com>
2020-12-30 17:01:01 +0000

    s1a5 edgecases fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 l = abs(number) % 10
+if (number < 0):
+    l = -1 * l
 if l > 5:
     print("Last digit of {} is {} and is greater than 5"
           .format(number, l))
-elif last_number == 0:
+elif l == 0:
     print("Last digit of {} is {} and is 0".format(number, l))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0"
