60c12f8a9ab397a70e8a5bee4f3df28740cf9dd4
jdanielue <jdurregoe@gmail.com>
2021-01-02 15:48:04 -0500

    s10a0

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    mayor = list()
+    if a_dictionary is None:
+        return None
+    for actual in a_dictionary.items():
+        for comparado in a_dictionary.items():
+            if actual[1] < comparado[1]:
+                mayor = comparado[0]
+        pass
+return mayor
