35d8329e0681bda99edd8ba6ec861fd64bfcc617
jdanielue <jdurregoe@gmail.com>
2021-01-04 15:42:47 -0500

    s4a5

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     nueva_lista = []
-    if len(my_list_1) > len(my_list_2):
-        tamano = len(my_list_1)
-    else:
-        tamano = len(my_list_2)
-
-    for numero in range(tamano):
+    for numero in range(list_length):
         try:
             nuevo_elemento = my_list_1[numero]/my_list_2[numero]
         except ZeroDivisionError:
