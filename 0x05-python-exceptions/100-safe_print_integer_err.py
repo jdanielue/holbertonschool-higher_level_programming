0bd0b411d2ffd02049b51d71bca7a94df56cfc82
jdanielue <jdurregoe@gmail.com>
2021-01-04 16:29:13 -0500

    s100a0

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        sys.stderr.write("Exception: Unknown format code 'd' for object of type 'str' Holberton is not an integer \n")
+        return False
+    except ValueError:
+        sys.stderr.write("Exception: Unknown format code 'd' for object of type 'str' Holberton is not an integer \n")
+        return False
+    else:
+        return True
