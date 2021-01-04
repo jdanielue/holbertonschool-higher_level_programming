1a733d48d7ab8e492787fddc4ccea525b758e9d8
jdanielue <jdurregoe@gmail.com>
2021-01-04 13:13:11 -0500

    s0s1 a1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,12 @@
 def safe_print_list(my_list=[], x=0):
     new_element = ""
     contador = 0
-
     for elemento in my_list:
-        if  contador < x:
+        if contador < x:
             try:
                 new_element += elemento
             except TypeError:
                 new_element += str(elemento)
             contador += 1
-    print (new_element)
+    print(new_element)
     return contador
