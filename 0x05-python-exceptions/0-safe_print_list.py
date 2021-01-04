f3846435226c40191749f97ee20d35830fb344f2
jdanielue <jdurregoe@gmail.com>
2021-01-04 13:04:40 -0500

    s0s1 a0 Readme file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    new_element = ""
+    contador = 0
+
+    for elemento in my_list:
+        if  contador < x:
+            try:
+                new_element += elemento
+            except TypeError:
+                new_element += str(elemento)
+            contador += 1
+    print (new_element)
+    return contador
