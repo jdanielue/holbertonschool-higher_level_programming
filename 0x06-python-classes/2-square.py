f08095a35312e44a75b57be77739abb16452de3c
jdanielue <jdurregoe@gmail.com>
2021-01-06 11:21:11 -0500

    s2a0 with pep8

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module has the definition of  a Square class"""
+
+
+class Square():
+    """ Model of a square """
+    def __init__(self, size=0):
+        """ Constructor for Square Method """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
