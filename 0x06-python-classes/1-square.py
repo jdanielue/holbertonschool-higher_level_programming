c0a4c438780d045267d9512630593fdc6642d80c
jdanielue <jdurregoe@gmail.com>
2021-01-06 11:18:08 -0500

    s1a1 with pep8

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module has the definition of  a Square class"""
+
+
+class Square():
+    """ Model of a square """
+    def __init__(self, size):
+        """ Constructor for Square Method """
+        self.__size = size
