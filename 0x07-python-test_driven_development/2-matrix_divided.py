da74cb29fd43c690fa6eab34cbfebafcfc704d9b
jdanielue <jdurregoe@gmail.com>
2021-01-09 19:24:54 -0500

    s2a1 s5a0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,30 +15,27 @@ def matrix_divided(matrix, div):
         matrix ([list]): [matrix ]
         div ([integer]): [number to be divideed]
     """
-    
-    if type(div) != int or type(div) != float:
+
+    if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
-        raise TypeError("division by zero")
-
+        raise ZeroDivisionError("division by zero")
 
     mat_div = []
     for row in matrix:
         dato = []
         for column in row:
-            if type(column) != int:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            if  type(column) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if type(column) != int and type(column) != float:
+                raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
             dato += [round((column / div), 2)]
         mat_div += [dato]
-    
+
     for fila in matrix:
         largo = len(fila)
         for fila2 in matrix:
             if largo != len(fila2):
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix must have the\
+ same size")
 
     return mat_div
-
-
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""[Prints a text with 2 new lines after each of these characters]
+"""
+
+
+def text_indentation(text):
+    """[summary]
+
+    Args:
+        text ([text]): [text to be printed]
+    """
+    new_text = ""
+    salto = "\n\n"
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    for position in range(0, len(text)):
+        if text[position - 1] == "." or text[position - 1] == "?"\
+        or text[position - 1] == ":":
+            continue
+        new_text += text[position]
+        if text[position] == "." or text[position] == "?"\
+        or text[position] == ":":
+            new_text = new_text + salto
+
+    print(new_text, end="")
