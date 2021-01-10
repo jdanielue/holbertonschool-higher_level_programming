9b124c01425b030ecf70f60856868f29af97265b
jdanielue <jdurregoe@gmail.com>
2021-01-10 13:40:29 -0500

    s6a1 all good

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,6 +12,18 @@ class TestMaxInteger(unittest.TestCase):
     def test_normal_case(self):
       self.assertAlmostEqual(max_integer([2, 5, 3, 8, 6]), 8)
 
+    def test_normal_case_end(self):
+      self.assertAlmostEqual(max_integer([2, 5, 3, 8, 10]), 10)
+    
+    def test_normal_case_beg(self):
+      self.assertAlmostEqual(max_integer([10, 5, 3, 8, 6]), 10)
+    
+    def test_normal_case_neg(self):
+      self.assertAlmostEqual(max_integer([10, 5, 3, -8, 6]), 10)
+
+    def test_normal_case_all_neg(self):
+      self.assertAlmostEqual(max_integer([-10, -5, -3, -8, -6]), -3)
+
 if __name__ == '__main__':
     unittest.main()
   
