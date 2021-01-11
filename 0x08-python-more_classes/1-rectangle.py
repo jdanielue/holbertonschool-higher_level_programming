87dd64cb8861247948f70370d80300d2bb8f63e5
jdanielue <jdurregoe@gmail.com>
2021-01-11 18:59:00 -0500

    s1a3 pep8 doc

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,6 +3,8 @@
 
 
 class Rectangle():
+    """[class rectangle]"""
+
     def __init__(self, width=0, height=0):
         """[class Rectangle that defines a rectangle]
 
@@ -10,5 +12,29 @@ class Rectangle():
             width (int, optional): [description]. Defaults to 0.
             height (int, optional): [description]. Defaults to 0.
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
