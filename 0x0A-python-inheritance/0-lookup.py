79e845344d747b2a67f7a8aa4cb50484e1b9174d
jdanielue <jdurregoe@gmail.com>
2021-01-19 18:30:31 -0500

    s0a1 + pep8 + readme

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """[returns the list of available attributes and methods of an object]"""
 
+
 def lookup(obj):
     """[returns the list of available attributes and methods of an object]
 
@@ -9,5 +10,5 @@ def lookup(obj):
 
     Returns:
         [list]: [list of attributes]
-    """    
+    """
     return dir(obj)
