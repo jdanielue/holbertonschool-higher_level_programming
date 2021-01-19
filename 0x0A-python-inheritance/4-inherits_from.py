0cdc74314d140bd8e2392833433a37f194912a4c
jdanielue <jdurregoe@gmail.com>
2021-01-19 18:38:52 -0500

    s4a1 doc pep8 all done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""True if the object is an instance"""
+
+
 def inherits_from(obj, a_class):
+    """True if the object is an instance"""
     if type(obj) is a_class:
         return False
     return issubclass(type(obj), a_class)
