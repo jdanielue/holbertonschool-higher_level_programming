7505857f1ddb4f0ff80a8bca060a0f6985931f5c
jdanielue <jdurregoe@gmail.com>
2021-01-19 18:34:32 -0500

    s2a1 pep8 all done

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-    """[True if the object is exactly an instance of the specified class]
-    """
+"""[True if the object is exactly an instance of the specified class]"""
+
 
 def is_same_class(obj, a_class):
     """[True if the object is exactly an instance of the specified class]
