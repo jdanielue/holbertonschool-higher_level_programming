16bba8f631aca594cc08d0915ca98938e5b8f8c8
jdanielue <jdurregoe@gmail.com>
2021-01-19 18:39:32 -0500

    s3a1 doc pep8 all done

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""True if the object is an instance"""
+
+
 def is_kind_of_class(obj, a_class):
+    """True if the object is an instance"""
     return isinstance(obj, a_class)
