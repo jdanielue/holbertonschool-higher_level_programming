ed090a95c73e972322b94d7dbceaa0e8da60aef4
jdanielue <jdurregoe@gmail.com>
2021-01-19 23:28:25 -0500

    s10a0

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""[empty class BaseGeometry]"""
+
+
+class BaseGeometry():
+    """[empty class BaseGeometry]"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """[nteger validator]"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """[empty class rectangle]"""
+    def __init__(self, width, height):
+        """[constructor new class]"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(BaseGeometry):
+    """[empty class rectangle]"""
+    def __init__(self, size):
+        """[constructor new class]"""
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size * self.__size
