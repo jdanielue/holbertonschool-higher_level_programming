243d8936ab8ca8bdcecfd6f12014c731a78f3278
jdanielue <jdurregoe@gmail.com>
2021-01-19 23:21:43 -0500

    s8a3 padre clase

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -22,5 +22,5 @@ class Rectangle(BaseGeometry):
         """[constructor new class]"""
         self.__width = width
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        BaseGeometry.integer_validator("width", width)
+        BaseGeometry.integer_validator("height", height)
