213f4a9625b971e78ea1293b7fa2590dfd603848
jdanielue <jdurregoe@gmail.com>
2021-01-19 23:17:28 -0500

    s9a1 pep8 working properly

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-    
+
     def area(self):
         return self.__width * self.__height
 
