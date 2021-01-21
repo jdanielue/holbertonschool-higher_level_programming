167bb070174acba61bb4bb9f90392747f53da73b
jdanielue <jdurregoe@gmail.com>
2021-01-20 21:53:05 -0500

    s0a1 checkers fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     """[ function that reads a text file (UTF8)]"""
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, encoding='utf-8') as file:
         for line in file:
             print(line, end='')
-        print()
+
