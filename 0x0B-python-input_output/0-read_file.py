3378fa4a402d9583b9cee312bc279e3d357825e1
jdanielue <jdurregoe@gmail.com>
2021-01-20 21:47:45 -0500

    s0s1s2 a1 doc ok pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""[ function that reads a text file (UTF8)]"""
 def read_file(filename=""):
+    """[ function that reads a text file (UTF8)]"""
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
