d8053b213af496d1b44a07bdfe814b22fe8a9786
jdanielue <jdurregoe@gmail.com>
2021-01-20 21:48:55 -0500

    s0s1s2 a2 doc ok pep8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """[ function that write a text file (UTF8)]"""
+
+
 def write_file(filename="", text=""):
     """[ function that write a text file (UTF8)]"""
     with open(filename, "x", encoding='utf-8') as file:
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """[ function that write a text file (UTF8)]"""
+
+
 def append_write(filename="", text=""):
     """[ function that write a text file (UTF8)]"""
     with open(filename, "a", encoding='utf-8') as file:
