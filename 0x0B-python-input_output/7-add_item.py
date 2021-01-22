56810fd4edb3ec903382475f59d14b9b7a92a8ea
jdanielue <jdurregoe@gmail.com>
2021-01-22 14:45:11 -0500

    s7a3 everything is ok 2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,11 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+try:
+    new_list = load_from_json_file("add_item.json")
 
-new_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    new_list = []
+
 new_list += sys.argv[1:]
 save_to_json_file(new_list, "add_item.json")
