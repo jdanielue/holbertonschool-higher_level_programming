641605d5be2f1f1c259700c8533d0cd55550b372
jdanielue <jdurregoe@gmail.com>
2021-01-26 22:07:14 -0500

    unit tests empty

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" unit test for files """
+
+
+
+import unittest
+import json
+import pep8
+import inspect
+from models.base import Base, __doc__
+from models.rectangle import Rectangle
+from models.square import Square
