3259fc0c1bb579b5b8ac15746e62d2f61409a7cd
jdanielue <jdurregoe@gmail.com>
2021-03-22 15:47:04 -0500

    s11a2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,7 @@ array.splice(0, 2);
 console.log(array);
 
 if (array.length <= 1) {
-    console.log(0)
-}
-else {
-    console.log(array[array.length - 2]);
+  console.log(0);
+} else {
+  console.log(array[array.length - 2]);
 }
