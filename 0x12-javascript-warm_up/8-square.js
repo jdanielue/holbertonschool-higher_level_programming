ef9347b48fea159bf476cee29d04d36f7888d933
jdanielue <jdurregoe@gmail.com>
2021-03-22 14:12:35 -0500

    s8a3

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-let array =[];
-let letra = "X";
+let array = [];
+const letra = 'X';
 
 for (let largo = 1; largo <= `${process.argv[2]}`; largo++) {
-    array = [];
-    for (let ancho = 1; ancho <= `${process.argv[2]}`; ancho++) {
+  array = [];
+  for (let ancho = 1; ancho <= `${process.argv[2]}`; ancho++) {
     array.push(letra);
-    }
-    console.log(array.join(','));
+  }
+  console.log(array.join(''));
 }
