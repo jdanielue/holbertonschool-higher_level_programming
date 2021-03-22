4b9590d31ec33d52254f9d20d0f08bc65a9eb1c3
jdanielue <jdurregoe@gmail.com>
2021-03-22 14:10:58 -0500

    s8a0

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+let array =[];
+let letra = "X";
 
 for (let largo = 1; largo <= `${process.argv[2]}`; largo++) {
+    array = [];
     for (let ancho = 1; ancho <= `${process.argv[2]}`; ancho++) {
-        process.stdout.write('X');
+    array.push(letra);
     }
-    process.stdout.write('\n');
+    console.log(array.join(','));
 }
