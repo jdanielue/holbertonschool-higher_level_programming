03c0c37276f5538be6ebfc1c8c9f796597687a9c
jdanielue <jdurregoe@gmail.com>
2021-03-22 12:56:02 -0500

    s5a0

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-console.log(typeof(`${process.argv[2]}`));
-console.log(Number(`${process.argv[2]}`));
+
+if (isNaN(Number(`${process.argv[2]}`)))
+{
+    console.log('Not a number');
+}
+else
+{
+    console.log('My number: ' + `${process.argv[2]}`);
+}
