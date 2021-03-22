8a41ba627c1b0d8c86df291d4e2c14ae2a88cd0d
jdanielue <jdurregoe@gmail.com>
2021-03-22 15:46:07 -0500

    s11a1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 let array = process.argv;
 array = array.sort();
-array.splice(0, 3);
-console.log(array[array.length - 2]);
+array.splice(0, 2);
+console.log(array);
+
+if (array.length <= 1) {
+    console.log(0)
+}
+else {
+    console.log(array[array.length - 2]);
+}
