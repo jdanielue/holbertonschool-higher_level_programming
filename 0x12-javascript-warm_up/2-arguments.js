d8356c6f245426e9dbbc70da61dd66f600015065
jdanielue <jdurregoe@gmail.com>
2021-03-22 12:10:52 -0500

    s2a1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-if (process.argv[3])
-{
-    console.log("Arguments found");
-}
-else if (process.argv[2])
-{
-    console.log("Argument found");
-}
-else
-{
-    console.log("No argument");
+if (process.argv[3]) {
+  console.log('Arguments found');
+} else if (process.argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
