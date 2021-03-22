382dfecfd28be59d84abd77aa5e388b2e6cef032
jdanielue <jdurregoe@gmail.com>
2021-03-22 14:14:49 -0500

    s8a4

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,6 +2,10 @@
 let array = [];
 const letra = 'X';
 
+if (isNaN(Number(`${process.argv[2]}`))) {
+    console.log('Missing size');
+}
+
 for (let largo = 1; largo <= `${process.argv[2]}`; largo++) {
   array = [];
   for (let ancho = 1; ancho <= `${process.argv[2]}`; ancho++) {
