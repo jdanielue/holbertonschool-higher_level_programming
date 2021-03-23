352e75cffe5d91cde2c9a9a65434ea971bef9d0d
jdanielue <jdurregoe@gmail.com>
2021-03-23 15:31:07 -0500

    s4a0

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const forma = 'X';
+    for (let contador = 1; contador <= this.height; contador++) {
+      console.log(forma.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
