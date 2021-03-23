dee5c05c299bf863c83b94f1a2637f8f896c4599
jdanielue <jdurregoe@gmail.com>
2021-03-23 14:57:51 -0500

    s3a1

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (h, w) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    this.print = function () {
+      const forma = 'X';
+      for (let contador = 1; contador <= this.width; contador++) {
+        console.log(forma.repeat(this.height));
+      }
+    };
+  }
+};
