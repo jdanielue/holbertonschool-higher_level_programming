6ae01344d891b9b8fd7881df743a5aca843345de
jdanielue <jdurregoe@gmail.com>
2021-03-23 20:47:34 -0500

    s8a0

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let a;
+for (let count= 0; count< Math.floor(list.length / 2); i++) {
+    a = list[count];
+    list[count] = list[list.length - count- 1];
+    list[list.length - count- 1] = a;
+}
+return (list);
+};
+exports.esrever = function (list) {
