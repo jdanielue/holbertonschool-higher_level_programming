c5799093418681a2f1e2fa5ddd26f873eb947477
jdanielue <jdurregoe@gmail.com>
2021-03-23 20:51:04 -0500

    s6a0

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const Square1 = require('./5-square');
+
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let count = 0; count < this.height; count++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
