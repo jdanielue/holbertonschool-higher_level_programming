f177b3c1fb8a0a3e0d336b052c3d968f34aa52c6
jdanielue <jdurregoe@gmail.com>
2021-03-23 20:52:55 -0500

    s7a3

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-let ocr = 0;
-exports.logMe = function (item) {
-  console.log(`${ocr}:`, item);
-  ocr++;
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) count++;
+  }
+  return (count);
 };
