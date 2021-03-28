f48248596f73e48e3dea2dc48ea299c08e81f9e3
jdanielue <jdurregoe@gmail.com>
2021-03-28 09:38:52 -0500

    other files

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,6 @@ array.splice(0, 2);
 
 if (array.length <= 1) {
   console.log(0);
-} else if (array[array.length - 2] === array[array.length - 1])
-    console.log(undefined);
-else {
+} else if (array[array.length - 2] === array[array.length - 1]) { console.log(undefined); } else {
   console.log(array[array.length - 2]);
 }
