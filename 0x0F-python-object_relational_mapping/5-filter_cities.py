0e9d8dc51d8a19eaf3d0aca15acbe676e0577cc7
jdanielue <jdurregoe@gmail.com>
2021-03-28 17:50:56 -0500

    s5a1d1

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3                                                                                      
-"""                                                                                                                                                                             
+#!/usr/bin/python3
+"""
 script that lists all states from the database hbtn_0e_0_usa
- 
 """
 import MySQLdb
 import sys
@@ -10,7 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = '{}'" .format(sys.argv[4]))
+    cur.execute("SELECT cities.name FROM cities JOIN states\
+                 ON cities.state_id = states.id\
+                 WHERE states.name = '{}'" .format(sys.argv[4]))
     query_rows = cur.fetchall()
     lista = []
     for row in query_rows:
