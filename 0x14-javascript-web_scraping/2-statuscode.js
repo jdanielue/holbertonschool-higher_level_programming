34b02d957dcec5b65bf5fb71018ede697f5b2f45
jdanielue <jdurregoe@gmail.com>
2021-05-04 13:41:58 -0500

    s2a6

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[2], function (response) {
+request(process.argv[2], function (err, response) {
+  if (err) {
+    return console.log(err);
+  }
   console.log('code:', response && response.statusCode);
 });
